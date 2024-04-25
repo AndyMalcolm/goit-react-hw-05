7e881e5b5b9776fd6f57e26d09f11255f21213f5
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2024-04-25 12:51:03 +0300

    hm

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://api.themoviedb.org/3/";
 
-const API_KEY = "b101889254ba0539f51aa4769f4bbe68";
+const API_KEY = "b6ee193cbf02b64ce906e1c77db45752";
 const token =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTAxODg5MjU0YmEwNTM5ZjUxYWE0NzY5ZjRiYmU2OCIsInN1YiI6IjY2MTI4ZmVkNjdkY2M5MDE0OTliNjQyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3j5bx9VjOiJ_C4B8L53NieFOKRM7U3R4NzFijqxC9Rk";
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmVlMTkzY2JmMDJiNjRjZTkwNmUxYzc3ZGI0NTc1MiIsInN1YiI6IjY2MmEyNjVmZGM4NjQ3MDBhZTUyNmJlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ecW8Rd1h69bQnjhc9OKhbMcQOdKfnM1IMZx1vCger3Q";
 
 axios.defaults.headers.common["Authorization"] = token;
 
